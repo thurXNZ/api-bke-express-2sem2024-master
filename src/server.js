ede74e81502a5fa4ffc8f7b565f12040e96ad98b
thurXNZ <arthur.mello@aluno.ifsp.edu.br>
2024-08-20 17:34:43 -0300

    fix production host log

--- src/server.js
+++ src/server.js
@@ -9,5 +9,5 @@ app.use('/auth', authRouter)
 app.use('/account', accountRouter)
 
 app.listen(PORT, () => {
-    console.log(`Servidor Rodando no Ambiante ${ ENVIRONMENT } em ${HOST}:${PORT}`)
+    console.log(`Servidor Rodando no Ambiante ${ ENVIRONMENT } em ${ ENVIRONMENT == 'production' ? HOST : HOST+':'+PORT }`)
 })
