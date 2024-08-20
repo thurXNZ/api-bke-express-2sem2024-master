a95ac1c88e9826d899c2462c15e36fac92f6221e
thurXNZ <arthur.mello@aluno.ifsp.edu.br>
2024-08-20 17:18:31 -0300

    add HOST and PORT ENV

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import authRouter from './routers/authRouter.js'
 import accountRouter from './routers/accountRouter.js'
-import { ENVIRONMENT } from './config.js'
+import { ENVIRONMENT, PORT, HOST } from './config.js'
 
 const app = express()
 
 app.use('/auth', authRouter)
 app.use('/account', accountRouter)
 
-app.listen(3000, () => {
-    console.log(`Servidor Rodando no Ambiante ${ ENVIRONMENT }`)
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando no Ambiante ${ ENVIRONMENT } em ${HOST}:${PORT}`)
 })
